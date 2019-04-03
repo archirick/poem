9e548fe2a30af286ddf2ca354bd63ed6b69ef70a
archirick <48961843+archirick@users.noreply.github.com>
2019-04-03 19:00:45 +0530

    Add files via upload

---
+++ poem.py
@@ -0,0 +1,7 @@
+
+print("Twinke,twinkle little star,")
+print("\t How i wonder what you are!")
+print("\t\tUp above so world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinke, twinkle, little star,")
+print("\tHow i wonder what you are")
